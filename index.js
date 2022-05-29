4ac6028e119363baed664a3a4d7b4198ad1bf1c0
Reback <gregreback@gmail.com>
2022-05-29 18:47:47 -0400

    Hi

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let number = document.getElementById("number").innerText;
 console.log(number);
-number = 32;
+number = 1;
 document.getElementById("number").innerText = number;
 
 console.log(number);
@@ -17,6 +17,19 @@ function numberEl() {
   let meters = 0.3048 * number;
 
   document.getElementById("meters").innerText = meters;
+
+  let liters = number;
+  document.getElementById("litersEl").innerText = liters;
+  let gallons = number * 0.264172;
+  document.getElementById("gallons").innerText = gallons;
+
+  document.getElementById("gallonsQ").innerText = number;
+  document.getElementById("liters").innerText = number * 3.78541;
+
+  document.getElementById("kilos").innerText = number;
+  document.getElementById("pounds").innerText = number * 2.205;
+  document.getElementById("poundsEl").innerText = number;
+  document.getElementById("kilo").innerText = number * 0.453592;
 }
 
 numberEl();
