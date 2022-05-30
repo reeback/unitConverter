087fc742d0256fdc3735c35b798ed2b8b565ed99
Reback <gregreback@gmail.com>
2022-05-30 11:00:11 -0400

    Hi

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
 let number = document.getElementById("number").innerText;
-console.log(number);
-number = 1;
+//console.log(number);
+number = 20;
 document.getElementById("number").innerText = number;
 
-console.log(number);
+//console.log(number);
 
 function numberEl() {
   // console.log(meters);
   // console.log(meters);
   document.getElementById("metersEl").innerText = number;
-  let feet = 3.2808 * number;
-  document.getElementById("feet").innerText = feet;
+  let feet = 3.280839895 * number;
+  document.getElementById("feet").innerText = feet.toFixed(3);
   let feetQ = number;
   document.getElementById("feetQ").innerText = feetQ;
   // 0.3048
-  let meters = 0.3048 * number;
+  let meters = 0.3048 * number.toFixed(3);
 
-  document.getElementById("meters").innerText = meters;
+  document.getElementById("meters").innerText = meters.toFixed(3);
 
   let liters = number;
   document.getElementById("litersEl").innerText = liters;
   let gallons = number * 0.264172;
-  document.getElementById("gallons").innerText = gallons;
+  document.getElementById("gallons").innerText = gallons.toFixed(3);
 
   document.getElementById("gallonsQ").innerText = number;
-  document.getElementById("liters").innerText = number * 3.78541;
+  let litersQQ = number * 3.785412;
+  document.getElementById("liters").innerText = litersQQ.toFixed(3);
 
   document.getElementById("kilos").innerText = number;
-  document.getElementById("pounds").innerText = number * 2.205;
+  let poundsQQ = number * 2.20462262185;
+  document.getElementById("pounds").innerText = poundsQQ.toFixed(3);
   document.getElementById("poundsEl").innerText = number;
-  document.getElementById("kilo").innerText = number * 0.453592;
+  let kiloQQ = number * 0.45359237;
+  document.getElementById("kilo").innerText = kiloQQ.toFixed(3);
 }
 
 numberEl();
